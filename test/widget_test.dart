0cc90adba2d375c598e2188cb387705edcde41da
Duy Anh <anh.ld3@kiotviet.com>
2026-04-14 11:49:32 +0700

    .

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:medmate_stt/app/app.dart';
+import 'package:medmate_stt/src/app.dart';
 
 void main() {
   testWidgets('MedMateApp smoke test', (WidgetTester tester) async {
